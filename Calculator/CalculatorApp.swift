76b1a33a2a70366f6c5c5d8282d223e7591aa388
Jasmine Teutsch <jaspeach@outlook.com>
2022-02-01 21:56:44 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jasmine Tetusch on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
